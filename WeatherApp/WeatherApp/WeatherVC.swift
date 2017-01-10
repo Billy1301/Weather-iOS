91ace759ff868ed05eeb27f70e049de2b2716c9b
Billy <mr.bill.ly@gmail.com>
2017-01-10 15:23:30 -0800

    fix thread errors

---
+++ WeatherApp/WeatherApp/WeatherVC.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Billy on 1/10/17.
+//  Copyright © 2017 Billy. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var curentTempLabel: UILabel!
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+   
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+
+
+}
+
